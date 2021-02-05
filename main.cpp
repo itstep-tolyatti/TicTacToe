6b7f257f5d88ef4ebef3e9319c23420973760f5a
anst-foto <starininandrey@gmail.com>
2021-02-05 17:24:03 +0300

    Добавление вывода игрового поля

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    system("chcp 65001");
+
+    const int SIZE = 3;
+    char board[SIZE][SIZE] = {{' ', ' ', ' '},
+                              {' ', ' ', ' '},
+                              {' ', ' ', ' '}};
+
+    cout << "+++ Крестики-Нолики +++" << endl;
+    cout << endl;
+
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            cout << "|" << board[i][j] << "|";
+        }
+        cout << endl;
+    }
 }
